6738c86228bbbc0bdb5eea3a7f2ecfafdd9d1903
ParagMhrjn <chandra_parag@hotmail.com>
2025-07-09 21:08:04 +0200

    setup connection and env for mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,17 @@
 import express from "express"
 import noteRoutes from "./routes/noteRoutes.js"
+import { connectDB } from "./config/db.js"
+import dotenv from "dotenv"
 const app = express()
-
+const PORT = process.env.PORT || 5000
+dotenv.config();
 // prefixing api routing endpoints
 app.use("/api/notes", noteRoutes)
 
+connectDB()
 
 
 
-
-app.listen(5000, () => {
-    console.log("Server started on PORT: 5000");
+app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
 });
