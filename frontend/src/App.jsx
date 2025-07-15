eb7fc53bf04a6ada742a1e553be86784d0a45ac9
ParagMhrjn <chandra_parag@hotmail.com>
2025-07-15 20:57:10 +0200

    toaster popup message added and setup tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
+import {toast} from 'react-hot-toast'
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success("Welcome! It is connected Successfully")} className="text-red-400">Click Me</button>
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/create' element={<CreatePage />}></Route>
