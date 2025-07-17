28af9f9e12ba491cf170ee6e3efdb4d780275661
ParagMhrjn <chandra_parag@hotmail.com>
2025-07-17 14:20:40 +0200

    added ratelimiter component to showcase and alert about limitation of usage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import NavBar from '../components/NavBar'
-
+import { useState } from 'react'
+import RateLimitedUI from "../components/RateLimitedUI"
 const HomePage  = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
   return (
     <div className='min-h-screen'>
       <NavBar/>
+
+      {isRateLimited && <RateLimitedUI/>}
     </div>
   )
 }
