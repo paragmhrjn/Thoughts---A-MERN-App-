ee084bc3f381bf85ed7fc8bc930d661237f26998
ParagMhrjn <chandra_parag@hotmail.com>
2025-07-17 20:35:36 +0200

    added delete functionality to be able to delete a note

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,23 @@
 import { Link } from 'react-router'
 import { PenSquareIcon, Trash2Icon } from 'lucide-react'
-
+import toast from 'react-hot-toast'
 import { formatDate } from '../lib/utils'
-const NoteCard = ({ note }) => {
+import api from '../lib/axios'
+const NoteCard = ({ note, setNotes }) => {
+
+    const handleDelete = async (e, id) => {
+        e.preventDefault() // prevent from navigation on click
+
+        if (!window.confirm("Are you sure you want to delete this note?")) return
+        try {
+            await api.delete(`/notes/${id}`)
+            setNotes((prev) => prev.filter(note => note._id !==id ))// get rid of deleted note id and updater
+            toast.success("Note Deleted Successfully!!")
+        } catch (error) {
+            console.log("Error in handle delete", error)
+            toast.error("failed to delete note")
+        }
+    }
     return (
         <Link to={`/note/${note._id}`} className='card bg-base-200 hover:shadow-lg transition-all duration-200 border-t-4 border-solid'>
             <div className='card-body'>
@@ -17,7 +32,8 @@ const NoteCard = ({ note }) => {
                     </span>
                     <div className="flex items-center gap-1">
                         <PenSquareIcon className="size-4"></PenSquareIcon>
-                        <button className='btn btn-ghost btn-xs text-error'>
+                        <button className='btn btn-ghost btn-xs text-error'
+                        onClick={(e) => handleDelete(e, note._id)}>
                             <Trash2Icon className='size-4'/>
                         </button>
                     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import RateLimitedUI from "../components/RateLimitedUI"
 import toast from 'react-hot-toast'
 import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound'
 import api from '../lib/axios'
 const HomePage  = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -18,7 +19,7 @@ const HomePage  = () => {
         setIsRateLimited(false)
         // const res = await fetch("http://localhost:5000/api/notes")
         // const data = await res.json();
-        console.log(res)
+        // console.log(res)
       }catch (error) {
         console.log("Error Fetching data.")
         if(error.response?.status === 429) {
@@ -40,10 +41,12 @@ const HomePage  = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className='text-center text-primary py-10'> Loading Notes...</div>}
 
+        {/* conditional rendring */}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
-              <NoteCard key={note._id} note={note}/>
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
